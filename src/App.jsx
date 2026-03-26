a948233ac3f3c62e5fd85b22e78a70dd19092d15
Dahrius93 <dariolauretta93@gmail.com>
2026-03-26 20:25:52 +0100

    setup pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,69 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+  Login,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "product/:id",
+        element: <SingleProduct />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <div className="bg-red-500">
-        <h1>Main</h1>
-      </div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
